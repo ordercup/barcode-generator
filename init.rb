57e436c505a7adada41ecb829ac6648702280de8
Tim Matheson <me@timmatheson.com>
2011-11-03 07:18:56 -0700

    making bracode-generator bundler friendly

--- init.rb
+++ init.rb
@@ -1,5 +1,10 @@
 # Include hook code here
 require 'rubygems'
-require 'gbarcode'
+require 'bundler'
+if defined?(Bundler)
+  Bundler.require 'gbarcode' 
+else
+  require 'gbarcode'
+end
 require 'config'
 require 'barcode_generator'
